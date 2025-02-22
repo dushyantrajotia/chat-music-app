cf26ce53e3de03bf4423aaf080e3fd4a593c274d
Dushyant Rajotia <dushyantrajotia@gmail.com>
2025-02-22 09:15:05 -0800

    changes made to sync songs for both users

--- server.js
+++ server.js
@@ -1,27 +1,33 @@
 const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
+const cors = require("cors");
 
 const app = express();
+app.use(cors());
 const server = http.createServer(app);
 const io = new Server(server, {
-  cors: { origin: "*" },
+  cors: {
+    origin: "*",
+  },
 });
 
 io.on("connection", (socket) => {
-  console.log("A user connected");
+  console.log("User connected:", socket.id);
 
-  socket.on("sendMessage", (msg) => {
-    io.emit("receiveMessage", msg);
+  socket.on("sendMessage", (message) => {
+    io.emit("receiveMessage", message);
   });
 
-  socket.on("playSong", (url) => {
-    io.emit("syncSong", url);
+  socket.on("playSong", (songUrl) => {
+    io.emit("playSong", songUrl); // Broadcast song URL to all users
   });
 
   socket.on("disconnect", () => {
-    console.log("User disconnected");
+    console.log("User disconnected:", socket.id);
   });
 });
 
-server.listen(5000, () => console.log("Server running on port 5000"));
+server.listen(5000, () => {
+  console.log("Server running on port 5000");
+});
